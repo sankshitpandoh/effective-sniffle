e641128b1d6cbce6880769fc6cb97c330d2ad84c
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-06-16 15:05:40 +0530

    node server set up and proxy added in client package.json

--- client/src/index.js
+++ client/src/index.js
@@ -33,6 +33,23 @@ class App extends React.Component{
     })
   }
 
+    componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => console.log(res.express))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+  const response = await fetch('/api/hello');
+  const body = await response.json();
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+};
+
   render(){
     return(
       <>
